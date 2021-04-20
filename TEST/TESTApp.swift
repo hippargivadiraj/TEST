593938260f6a359cd447ac06029b62f70ecabbeb
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-04-19 18:19:06 -0700

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by Vadiraj Hippargi on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
